41842ca3d5d309bcd768145b88c012de27a34393
David-MarCle <martinezclementedavid@gmail.com>
2024-02-21 22:45:56 +0100

    Components

---
+++ FRONTEND/src/components/sections/Sections.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Sections.css' 
+import concert from "../../img/concertblue.png"
+const Sections = () => {
+  return (
+    <div className='sections__container'>
+      <img className='sections__img' src={concert} alt="" />
+      <div className="sections__cto">
+      <h2 className='sections__cto-title'>ghfhgfhgfhgfhgfgfhgfgfgfhg</h2>
+      <p className='sections__cto-text'>bnmxnzvxncmzxnvnvbcxmbvxnzbmnv</p>
+      </div>
+    </div>
+  )
+}
+
+export default Sections

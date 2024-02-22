db4f617a0b7c5d78becb1797556b16711df44d93
luisangelalvarez <149668358+luisangelalvarez@users.noreply.github.com>
2024-02-22 18:50:19 +0100

    Cypress funcionando

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,13 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // obtener la URL base de Cypress
+      const baseUrl = config.baseUrl;
+
+      // usar la URL base para configurar un listener de eventos
+      on('before:browser:launch', (browser, options) => {
+        console.log('Navegador a punto de lanzarse con la URL base:', baseUrl);
+      });
     },
   },
 });
--- cypress/e2e/login_spec.js
+++ cypress/e2e/login_spec.js
@@ -1,6 +1,6 @@
 describe('Mi primera prueba', () => {
     it('Visita la página de inicio', () => {
-      cy.visit('http://localhost:5173') // cambia esto a la URL de tu aplicación
+      cy.visit('http://localhost:5173/') // cambia esto a la URL de tu aplicación
     })
   })
   

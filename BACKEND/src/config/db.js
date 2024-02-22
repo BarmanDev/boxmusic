584ef1ce658708dda13a0de8b575d07e735aca8b
luisangelalvarez <149668358+luisangelalvarez@users.noreply.github.com>
2024-02-22 18:59:19 +0100

    Cypress running

--- BACKEND/src/config/db.js
+++ BACKEND/src/config/db.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const connection = mysql.createConnection({
     host: process.env.MYSQL_HOST || 'localhost',
     user: process.env.MYSQL_USER || 'root',
-    password: process.env.MYSQL_PASSWORD || 'password',
+    password: process.env.MYSQL_PASSWORD || '',
     database: process.env.MYSQL_DATABASE || 'db_music',
     port: process.env.MYSQL_PORT || 3306
 });

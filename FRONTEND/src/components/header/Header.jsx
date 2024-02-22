622fb53131647f291975c759464da951a7665359
David-MarCle <martinezclementedavid@gmail.com>
2024-02-22 14:08:38 +0100

    button header

--- FRONTEND/src/components/header/Header.jsx
+++ FRONTEND/src/components/header/Header.jsx
@@ -7,9 +7,16 @@ const Header = () => {
   return (
     <header className="header">
         
-            <img className="header__containerLogo-img" src= {logo} alt="" />  
-             <a href="#">jgjhghghfjhf</a>
-                 
+            <img className="header__img" src= {logo} alt="" /> 
+            <div className="header__button"> 
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-octagon-plus" width="28" height="28" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <path d="M12.802 2.165l5.575 2.389c.48 .206 .863 .589 1.07 1.07l2.388 5.574c.22 .512 .22 1.092 0 1.604l-2.389 5.575c-.206 .48 -.589 .863 -1.07 1.07l-5.574 2.388c-.512 .22 -1.092 .22 -1.604 0l-5.575 -2.389a2.036 2.036 0 0 1 -1.07 -1.07l-2.388 -5.574a2.036 2.036 0 0 1 0 -1.604l2.389 -5.575c.206 -.48 .589 -.863 1.07 -1.07l5.574 -2.388a2.036 2.036 0 0 1 1.604 0z" />
+  <path d="M9 12h6" />
+  <path d="M12 9v6" />
+</svg>
+             <a className="header__button-link" href="#">publicar anuncio</a>
+             </div>    
     </header>
   )
 }
